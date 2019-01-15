655341dda067cabe10a381ecb32336065db24fac
Eclectics <eclectics@Eclecticss-MacBook-Air-MM.local>
2019-01-15 13:06:15 +0300

    updates

--- FlightSchedule/util/Switcher.swift
+++ FlightSchedule/util/Switcher.swift
@@ -2,8 +2,8 @@
 //  Switcher.swift
 //  FlightSchedule
 //
-//  Created by Eclectics on 15/01/2019.
-//  Copyright © 2019 Eclectics. All rights reserved.
+//  Created by Morris on 15/01/2019.
+//  Copyright © 2019 Morris. All rights reserved.
 //
 
 import Foundation
